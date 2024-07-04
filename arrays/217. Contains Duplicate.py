bbdebb64a377807b1c1cb4085fde1d5c98c527f5
Anirban Sen <anirbansen2709@gmail.com>
2024-07-04 20:05:40 +0530

    Create 217. Contains Duplicate.py

---
+++ arrays/217. Contains Duplicate.py
@@ -0,0 +1,12 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        num_set = set()
+        for num in nums:
+            if num in num_set:
+                return True
+            else:
+                num_set.add(num)
+        return False
+
+# TC - O(n)
+# SC - O(n)
