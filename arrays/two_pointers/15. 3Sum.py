f0d4261e47efc022c0f6dedaa9f43858a64b112f
Anirban Sen <anirbansen2709@gmail.com>
2024-07-06 13:00:31 +0530

    Create 15. 3Sum.py

---
+++ arrays/two_pointers/15. 3Sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        length = len(nums)
+        answer = []
+        for first in range(length - 2):
+            num_set = set()
+            for second in range(first + 1, length - 1):
+                left = - (nums[first] + nums[second])
+                if left in num_set:
+                    temp = [nums[first], nums[second], left]
+                    temp.sort()
+                    answer.append(tuple(temp))
+                num_set.add(nums[second])
+        return list(set(answer))
+
+# TC - O(n ** 2)
+# SC - O(1)
