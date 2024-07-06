27726102165c689ef3115ede19e63d3a026fae2b
Anirban Sen <anirbansen2709@gmail.com>
2024-07-06 13:17:03 +0530

    Create 17. Letter Combinations of a Phone Number.py

---
+++ recursion/17. Letter Combinations of a Phone Number.py
@@ -0,0 +1,33 @@
+class Solution:
+    def get_combinations(self, idx, answer, path):
+        if idx == self.length:
+            answer.append(str(path))
+            return
+        num = self.digits[idx]
+        for char in self.mapping[int(num)]:
+            print(path, char)
+            self.get_combinations(idx + 1, answer, path + char)
+        return
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        self.digits = digits
+        self.length = len(digits)
+        self.mapping = {
+            2 : "abc",
+            3 : "def",
+            4 : "ghi",
+            5 : "jkl",
+            6 : "mno",
+            7 : "pqrs",
+            8 : "tuv",
+            9 : "wxyz"
+        }
+        answer = []
+        path = ""
+        self.get_combinations(0, answer, path)
+        return answer
+
+# TC - O(length * m)
+# SC - O(m)
