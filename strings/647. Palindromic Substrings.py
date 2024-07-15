28fa6ce2c3bb0b486a0d773d2217902d46a07cc7
Anirban Sen <anirbansen2709@gmail.com>
2024-07-15 23:24:14 +0530

    Update 647. Palindromic Substrings.py

--- strings/647. Palindromic Substrings.py
+++ strings/647. Palindromic Substrings.py
@@ -1,1 +1,15 @@
+class Solution:
+    def get_counts(self, s, left, right):
+        count = 0
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            count += 1
+            left -= 1
+            right += 1
+        return count
 
+    def countSubstrings(self, s: str) -> int:
+        count = 0
+        for idx in range(len(s)):
+            count += self.get_counts(s, idx, idx)
+            count += self.get_counts(s, idx, idx + 1)
+        return count
