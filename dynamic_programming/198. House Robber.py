696b1824b157191a0b5c49056471d49563150107
Anirban Sen <anirbansen2709@gmail.com>
2025-09-07 15:30:07 +0530

    Update 198. House Robber.py
    Adding recursive solution

--- dynamic_programming/198. House Robber.py
+++ dynamic_programming/198. House Robber.py
@@ -1,3 +1,21 @@
+# Recursion Solution
+class Solution:
+    def get_max_amount(self, idx, nums):
+        if idx < 0 :
+            return 0
+        if idx == 0:
+            return nums[0]
+        rob = nums[idx] + self.get_max_amount(idx - 2, nums)
+        skip = self.get_max_amount(idx - 1, nums)
+        return max(rob, skip)
+
+    def rob(self, nums: List[int]) -> int:
+        length = len(nums)
+        return self.get_max_amount(length - 1, nums)
+
+# TC - O(2^n)
+# SC - O(n)
+
 class Solution:
     def get_max_amount(self, idx, nums, dp):
         if idx >= len(nums):
