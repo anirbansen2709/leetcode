051758bc55bcd4cf349a2301be1a0ea1ad4b64a2
Anirban Sen <anirbansen2709@gmail.com>
2026-03-19 15:33:16 +0530

    Enhance comments for maximum depth calculation
    Updated comments to clarify the base case and depth calculation.

--- trees/104. Maximum Depth of Binary Tree.py
+++ trees/104. Maximum Depth of Binary Tree.py
@@ -1,7 +1,9 @@
 # Given the root of a binary tree, return its maximum depth.
 # A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 
-# 
+# Base Case: If the node is empty, return 0.
+# Explore: Recursively calculate the maximum depth of the left and right subtrees.
+# Calculate: Return 1 (to count the current node) plus the greater of the two subtree depths.
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -21,4 +23,4 @@ class Solution:
         return self.traverse(root)
 
 # TC - O(n)
-# SC - O(logn)
+# SC - Worst case - O(n) Best Case - O(logn)
