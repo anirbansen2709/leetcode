f85e84cfcaf15112d662c35fd55586c35a6a764d
Anirban Sen <anirbansen2709@gmail.com>
2026-03-19 19:20:06 +0530

    Refactor cloneGraph method to use traverse function

--- graphs/dfs_bfs/133. Clone Graph.py
+++ graphs/dfs_bfs/133. Clone Graph.py
@@ -28,21 +28,19 @@ class Node:
 
 from typing import Optional
 class Solution:
-    def get_clone(self, node, old_to_new):
-        if node not in old_to_new:
-            clone = Node(node.val)
-            old_to_new[node] = clone
-            for nei in node.neighbors:
-                clone.neighbors.append(self.get_clone(nei, old_to_new))
-            return clone
-        else:
+    def traverse(self, node, old_to_new):
+        if node in old_to_new:
             return old_to_new[node]
+        clone = Node(node.val)
+        old_to_new[node] = clone
+        clone.neighbors = [self.traverse(neighbor, old_to_new) for neighbor in node.neighbors]
+        return clone
 
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if not node:
             return None
         old_to_new = dict()
-        return self.get_clone(node, old_to_new)
+        return self.traverse(node, old_to_new)
 
 # TC - O(V + E)
 # SC - O(V)
