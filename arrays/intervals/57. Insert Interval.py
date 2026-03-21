9be74c0a47532c1c9027b59d583a1e66fb62369d
Anirban Sen <anirbansen2709@gmail.com>
2026-03-21 17:24:30 +0530

    Refactor insert function for interval merging

--- arrays/intervals/57. Insert Interval.py
+++ arrays/intervals/57. Insert Interval.py
@@ -19,16 +19,16 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         answer = []
-        for idx, interval in enumerate(intervals):
-            if interval[0] > newInterval[1]:
+        for idx, (start, end)  in enumerate(intervals):
+            if newInterval[1] < start:
                 answer.append(newInterval)
                 return answer + intervals[idx:]
-            elif interval[1] < newInterval[0]:
-                answer.append(interval)
+            if newInterval[0] > end:
+                answer.append([start, end])
             else:
                 newInterval = [
-                    min(newInterval[0], interval[0]),
-                    max(newInterval[1], interval[1])
+                    min(start, newInterval[0]),
+                    max(end, newInterval[1])
                 ]
         answer.append(newInterval)
         return answer
